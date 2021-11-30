17d0a6aabd28a39264e4e2e91e73eb7d3b984783
David Abbey <davidabbey@earthlink.net>
2021-11-30 11:24:26 -0800

    fixed hoooks typo in saved pages

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useQuery, useMutation } from '@apollo/react-hoooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 
 import Auth from '../utils/auth';
 import { getMe, deleteBook } from '../utils/API';
