2b3ba89fc3334d8f5cdaafa4d60d304a3ae919c4
David Abbey <davidabbey@earthlink.net>
2021-11-30 11:43:46 -0800

    another typo fix

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,7 +9,7 @@ import { GET_ME } from '../utils/queries'
 import { REMOVE_BOOK } from '../utils/mutations'
 
 const SavedBooks = () => {
-  const { lodaing, data } = useQuery(GET_ME)
+  const { loading, data } = useQuery(GET_ME)
   const [removeBook, { error }] = useMutation(REMOVE_BOOK)
 
   const userData = data?.me || []
